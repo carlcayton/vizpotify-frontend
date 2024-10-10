708d68e36db4a0f41d3bc80a9417ee6890031c9f
Arian Carl Cayton <ariancayton@gmail.com>
2024-10-10 23:49:42 +0800

    refactor(Dashboard): Streamline component rendering and data fetching

--- src/pages/dashboard/[spotifyId].tsx
+++ src/pages/dashboard/[spotifyId].tsx
@@ -1,26 +1,15 @@
 import React, { useRef } from "react";
 import { useRouter } from "next/router";
 import { useQuery } from '@tanstack/react-query'
-import ProfileHeader from "components/ProfileHeader";
-import TopTracks from "components/TopTracks";
-import TopArtists from 'components/TopArtists';
-import Analytics from "components/Analytics";
-import NavBar from 'components/layout/NavBar';
-import CommentSection from 'components/CommentSection';
-import Modal from "components/common/Modal";
+import ProfileHeader from "@/components/ProfileHeader";
+import TopTracks from "@/components/TopTracks";
+import Analytics from "@/components/Analytics";
+import NavBar from '@/components/layout/NavBar';
+import CommentSection from '@/components/CommentSection';
+import Modal from "@/components/common/Modal";
 
-import {
-  getProfileHeaderData,
-  getUserTopArtist,
-} from "../../services/userService";
 import { useIsMobile } from "utils/detectScreenSize";
 
-interface ProfileHeaderData {
-  userDisplayName: string;
-  profilePictureUrl: string;
-  followedArtistCount: number;
-  followerCount: number;
-}
 
 export default function Dashboard() {
   const isMobile = useIsMobile();
@@ -28,22 +17,15 @@ export default function Dashboard() {
   const { spotifyId } = router.query;
   const modalRef = useRef<HTMLInputElement>(null);
 
-  const { data: profileHeaderData, isLoading: isLoadingProfile } = useQuery<ProfileHeaderData>({
-    queryKey: ['profileHeader', spotifyId],
-    queryFn: () => getProfileHeaderData(spotifyId as string),
-    enabled: !!spotifyId,
-  });
 
 
   return (
     <div className="flex flex-col justify-center w-full">
       <NavBar />
-      {profileHeaderData && (
-        <ProfileHeader {...profileHeaderData} />
-      )}
+      <ProfileHeader spotifyId={spotifyId as string} />
       <div className={`flex flex-col justify-center w-full px-10 bg-[#111827] ${isMobile ? 'sm:px-32' : 'md:px-64'}`}>
-        {/* <TopArtists userTopArtistsAllTimeRange={userTopArtistsAllTimeRange} /> */}
         <TopTracks spotifyId={spotifyId as string} />
+        {/* <TopArtists spotifyId={spotifyId as string} /> */}
         <Analytics spotifyId={spotifyId as string} />
         {/* {profileHeaderData && <CommentSection spotifyId={spotifyId as string} />} */}
       </div>
