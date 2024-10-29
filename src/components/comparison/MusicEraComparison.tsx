d3bf6856e93dec7cb9dd90804484cf8c4c90dd4d
Arian Carl Cayton <ariancayton@gmail.com>
2024-10-29 19:50:35 +0800

    add: Music Era Comparison

--- src/components/comparison/MusicEraComparison.tsx
+++ src/components/comparison/MusicEraComparison.tsx
@@ -67,7 +67,7 @@ const MusicEraComparison = ({
                 </div>
             </CardHeader>
             <CardContent>
-                <div className="h-[400px] w-full">
+                <div className="h-[350px] w-full">
                     <ResponsiveContainer width="100%" height="100%">
                         <BarChart
                             data={chartData}
